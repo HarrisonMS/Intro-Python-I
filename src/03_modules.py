9fa2481f8e42955d85430820ccce63d6da9183f7
Harrison Seaborn <54726103+HarrisonMS@users.noreply.github.com>
2020-09-25 13:27:01 -0700

    working on lists

--- src/03_modules.py
+++ src/03_modules.py
@@ -22,10 +22,14 @@ import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
 # Print the current process ID
+print(os.getpid())
 # YOUR CODE HERE
-
+print(os.getcwd())
 # Print the current working directory (cwd):
 # YOUR CODE HERE
 
 # Print out your machine's login name
 # YOUR CODE HERE
+
+
+print(os.getuid())
